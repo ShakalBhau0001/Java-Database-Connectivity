cc5351f079682ad3dad516f46f64accc50eb67d6
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-10-13 09:44:54 +0530

    CreatingTable.java

---
+++ Static/CreatingTable.java
@@ -0,0 +1,15 @@
+package staticJdbc;
+
+import java.sql.*;
+import oracle.jdbc.OracleDriver;
+
+public class CreatingTable {
+	public static void main(String[] args) throws Exception {
+		DriverManager.registerDriver(new OracleDriver());
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","test","test");
+		Statement st = con.createStatement();
+    st.executeUpdate("create table student(roll_number number primary key, student_name varchar2(20),college_fees number(5))");
+    System.out.println("Table Created Successfully..");
+    con.close();
+  }
+}
