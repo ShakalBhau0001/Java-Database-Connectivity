4929e8bbe9103a60f726a4ff2d4ab303c83c09f7
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-10-13 10:25:12 +0530

    DeletingRecord.java

--- Static/DeletingRecord.java
+++ Static/DeletingRecord.java
@@ -7,9 +7,9 @@ public class DeletingRecord {
 	public static void main(String[] args) throws Exception {
 		DriverManager.registerDriver(new OracleDriver());
 		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","test","test");
-    Statement stmt=con.createStatement();
-    stmt.executeUpdate("delete from student where roll_number=5");
-    System.out.println("Record Deleted Successfully..");
-    con.close();
-  }
+    	Statement stmt=con.createStatement();
+    	stmt.executeUpdate("delete from student where roll_number=5");
+    	System.out.println("Record Deleted Successfully..");
+    	con.close();
+  	}
 }
