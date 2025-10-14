e24e9cef6b5146cb773837a5d9d58c97847ca8d6
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-10-14 10:48:59 +0530

    CreatingTable.java

---
+++ Dynamic/CreatingTable.java
@@ -0,0 +1,15 @@
+package dynamicJdbc;
+
+import java.sql.*;
+import oracle.jdbc.OracleDriver;
+
+public class CreatingTable {
+	public static void main(String[] args) throws Exception {
+		DriverManager.registerDriver(new OracleDriver());
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","bca","college123");
+		Statement st = con.createStatement();
+		st.execute("create table BCA3(empid number primary key,empname varchar2(50),address varchar2(50),mobileno number(10),designation varchar2(20),department varchar2(20),salary number(7,2))");
+		System.out.println("Table Created");
+		con.close();
+	}
+}
