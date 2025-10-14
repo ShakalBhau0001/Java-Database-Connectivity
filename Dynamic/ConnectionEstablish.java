cf405b7957a4893baa265d8fdac11cf84b50baf3
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-10-14 10:52:17 +0530

    ConnectionEstablish.java

--- Dynamic/ConnectionEstablish.java
+++ Dynamic/ConnectionEstablish.java
@@ -6,8 +6,8 @@ import oracle.jdbc.OracleDriver;
 public class ConnectionEstablish {
 	public static void main(String[] args) throws Exception {
 		DriverManager.registerDriver(new OracleDriver());
-		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","bca","college123");
-    System.out.println("Connection Established Successfully");
+		Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE","test","test");
+    	System.out.println("Connection Established Successfully");
 		con.close();
 	}
 }
