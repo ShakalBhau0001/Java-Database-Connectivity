a373dc1ff98c93815c7869f3bb94fcc277a3a6f2
Shakal Bhau <160219957+ShakalBhau0001@users.noreply.github.com>
2025-10-14 10:51:09 +0530

    DeletingRecord.java

---
+++ Dynamic/DeletingRecord.java
@@ -0,0 +1,35 @@
+package dynamicJdbc;
+
+import java.sql.*;
+import java.util.Scanner;
+import oracle.jdbc.OracleDriver;
+
+public class DeletingRecord {
+    public static void main(String[] args) throws Exception {
+        DriverManager.registerDriver(new OracleDriver());
+        Connection con = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:XE", "test", "test");
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter Employee Id to delete:");
+            int id = sc.nextInt();
+            sc.nextLine();
+
+            PreparedStatement ps = con.prepareStatement("DELETE FROM BCA3 WHERE empid = ?");
+            ps.setInt(1, id);
+
+            int count = ps.executeUpdate();
+            if (count > 0) {
+                System.out.println("Record with ID " + id + " deleted successfully.");
+            } else {
+                System.out.println("No record found with ID: " + id);
+            }
+
+            System.out.println("Do you want to delete another record? [Y/N]");
+            String ch = sc.nextLine();
+            if (ch.equalsIgnoreCase("N"))
+                break;
+        }
+        con.close();
+    }
+}
